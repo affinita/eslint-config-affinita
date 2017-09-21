c662ac1409fea5055abab8f0bfc1f7919d1ef665
Florian Müller <florian.mueller81@gmx.de>
2017-09-22 00:03:03 +0200

    Initial ruleset and documentation

---
+++ ruleset/extras.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2017, Affinata. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  plugins: [
+    'extra-rules',
+  ],
+  rules: {
+    'extra-rules/no-commented-out-code': 2,
+    'extra-rules/no-single-line-objects': 1,
+    'extra-rules/potential-point-free': 1,
+  },
+};
